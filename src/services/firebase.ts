b3b99741d010038a67247821d9405c50c60be079
João <joaocruz375@gmail.com>
2023-08-16 15:46:54 -0300

    correção.

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,15 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-
-const firebaseConfig = {
+const firebaseApp = initializeApp({
   apiKey: "AIzaSyASEh7Akql0m-IAxG-oe1rKZPIeI8KTB6s",
   authDomain: "marry-invite-page.firebaseapp.com",
   projectId: "marry-invite-page",
   storageBucket: "marry-invite-page.appspot.com",
   messagingSenderId: "110536542869",
   appId: "1:110536542869:web:b3ec3b92806d92689ae99c"
-};
+});
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = getFirestore(firebaseApp);
